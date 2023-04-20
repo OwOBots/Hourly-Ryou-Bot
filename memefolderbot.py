943b5e78cdb8c46545d56e977780d3fbd2b7b208
Reisen <notyours066@gmail.com>
2023-04-19 21:08:20 -0700

    i hate compression

--- memefolderbot.py
+++ memefolderbot.py
@@ -54,20 +54,38 @@ def chooseRandomImage(directory="."):
     chosenImage = allImages[choice]
     return chosenImage
 
-def compress_image(path, quality=80):
+def compress_image(path, max_size=5):
     """
-    Compresses an image file to reduce its file size.
+    Compresses an image file to reduce its file size to less than the specified maximum size.
 
     Args:
         path (str): The path to the image file to be compressed.
-        quality (int, optional): The compression quality to use (0-100). Defaults to 80.
+        max_size (int, optional): The maximum file size in MB. Defaults to 5.
 
     Returns:
         str: The path to the compressed image file.
     """
-    with Image.open(path) as img:
-        img.save(path, optimize=True, quality=quality)
-    return path
+    compressed_path = path
+
+    # If the file size is already less than the specified maximum, return the original file path
+    if os.path.getsize(compressed_path) <= max_size * 1024 * 1024:
+        return compressed_path
+
+    quality = 80
+
+    # Iteratively compress the image with lower quality values until the file size is less than the specified maximum
+    while os.path.getsize(compressed_path) > max_size * 1024 * 1024:
+        quality -= 10
+        compressed_path = f"{os.path.splitext(path)[0]}_compressed.jpg"
+        with Image.open(path) as img:
+            img.save(compressed_path, optimize=True, quality=quality)
+
+        # If the quality value gets too low or the file size cannot be reduced below the maximum, break the loop
+        if quality < 10 or os.path.getsize(compressed_path) >= os.path.getsize(path):
+            compressed_path = path
+            break
+
+    return compressed_path
 
 def tweet():
     """
@@ -87,7 +105,9 @@ def tweet():
 
 def tweet():
     """
-    Tweet function that posts an image to Twitter and waits for an hour before posting again.
+    Tweet function that posts an image to Twitter and waits for an hour before posting again,
+    unless the compressed image file size is greater than 5MB, in which case it skips the tweet and
+    continues to the next iteration.
     :return: None
     """
     auth = tweepy.OAuth1UserHandler(
@@ -104,12 +124,22 @@ def tweet():
         path = asyncio.run(main())  # Get a random image from Gelbooru
         compressed_path = compress_image(path)  # Compress the image
         print("Compressed file path:", compressed_path)
-        media = api.media_upload(compressed_path,chunked=True)
+
+        # If the compressed file is larger than 5MB, skip the tweet and continue to the next iteration
+        if os.path.getsize(compressed_path) > 5 * 1024 * 1024:
+            print(f"Skipping tweet because compressed file size is {os.path.getsize(compressed_path) / (1024 * 1024):.2f} MB")
+            os.remove(compressed_path)  # Delete the compressed image file
+            print("Deleted compressed file:", compressed_path)
+            continue
+
+        media = api.media_upload(compressed_path, chunked=True)
         post_result = api.update_status(status=None, media_ids=[media.media_id_string])
         os.remove(compressed_path)  # Delete the compressed image file
         print("Deleted compressed file:", compressed_path)
-        print("Tweeted! Now sleeping for 1 hour...")
-        time.sleep(3600) # 3600 seconds = 1 hour
+        print("Tweeted!")
+        print("Now sleeping for 1 hour...")
+        time.sleep(3600)  # 3600 seconds = 1 hour
+
 
 
 if __name__ == "__main__":
